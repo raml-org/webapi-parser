d6a250621c0ab6fc3c6b9a94753d17013a6bd71e
Artem Kostiuk <postatum@gmail.com>
2018-11-28 14:04:57 +0200

    Write some scaladocs for webapi

--- shared/src/main/scala/webapi/WebApiParser.scala
+++ shared/src/main/scala/webapi/WebApiParser.scala
@@ -16,9 +16,12 @@ import scala.concurrent._
 import scala.scalajs.js.annotation._
 import ExecutionContext.Implicits.global
 
+
+/** Utility object meant for internal use */
 object WebApiParser {
   var initialized: Boolean = false
 
+  /** Initializes necessary plugins */
   def init(): ClientFuture[Unit] = {
     if(initialized) {
       val emptyFuture: Future[Unit] = Future {}
@@ -32,6 +35,7 @@ object WebApiParser {
     }
   }
 
+  /** Returns true if input represents path to local or remote file */
   def isPath(inp: String): Boolean = {
     (inp.startsWith("http://") || inp.startsWith("https://") || inp.startsWith("file:"))
   }
@@ -44,9 +48,16 @@ object WebApiParser {
   }
 }
 
+/** Provides methods for RAML 1.0 processing */
 @JSExportAll
 @JSExportTopLevel("WebApiParser.raml10")
 object Raml10 {
+
+  /** Parses RAML 1.0 content from string or url.
+    *
+    * @param urlOrContent File url/path or content string.
+    * @return Parsed model.
+    */
   def parse(urlOrContent: String): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       if(WebApiParser.isPath(urlOrContent)) {
@@ -57,24 +68,46 @@ object Raml10 {
     }).asClient
   }
 
+  /** Generates file with RAML 1.0 content.
+    *
+    * @param model Parsed model.
+    * @param url Generated file path.
+    */
   def generateFile(model: BaseUnit, url: String): ClientFuture[Unit] = {
     WebApiParser.chainAfterInit(() => {
       new Raml10Renderer().generateFile(model, url).asInternal
     }).asClient
   }
 
+  /** Generates string with RAML 1.0 content.
+    *
+    * @param model Parsed model.
+    * @return Generated string.
+    */
   def generateString(model: BaseUnit): ClientFuture[String] = {
     WebApiParser.chainAfterInit(() => {
       new Raml10Renderer().generateString(model).asInternal
     }).asClient
   }
 
+  /** Validates parsed RAML 1.0 model.
+    *
+    * @param model Parsed model.
+    * @return Validation report.
+    */
   def validate(model: BaseUnit): ClientFuture[ValidationReport] = {
     WebApiParser.chainAfterInit(() => {
       Core.validate(model, ProfileNames.RAML10, MessageStyles.RAML).asInternal
     }).asClient
   }
 
+  /** Resolves parsed RAML 1.0 model.
+    *
+    * Resolution process includes resolving references to all types, libraries, etc.
+    *
+    * @param model Parsed model.
+    * @return Resolved parsed model.
+    */
   def resolve(model: BaseUnit): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       Future {
@@ -85,9 +118,16 @@ object Raml10 {
   }
 }
 
+/** Provides methods for RAML 0.8 processing */
 @JSExportAll
 @JSExportTopLevel("WebApiParser.raml08")
 object Raml08 {
+
+  /** Parses RAML 0.8 content from string or url.
+    *
+    * @param urlOrContent File url/path or content string.
+    * @return Parsed model.
+    */
   def parse(urlOrContent: String): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       if(WebApiParser.isPath(urlOrContent)) {
@@ -98,24 +138,46 @@ object Raml08 {
     }).asClient
   }
 
+  /** Generates file with RAML 0.8 content.
+    *
+    * @param model Parsed model.
+    * @param url Generated file path.
+    */
   def generateFile(model: BaseUnit, url: String): ClientFuture[Unit] = {
     WebApiParser.chainAfterInit(() => {
       new Raml08Renderer().generateFile(model, url).asInternal
     }).asClient
   }
 
+  /** Generates string with RAML 0.8 content.
+    *
+    * @param model Parsed model.
+    * @return Generated string.
+    */
   def generateString(model: BaseUnit): ClientFuture[String] = {
     WebApiParser.chainAfterInit(() => {
       new Raml08Renderer().generateString(model).asInternal
     }).asClient
   }
 
+  /** Validates parsed RAML 0.8 model.
+    *
+    * @param model Parsed model.
+    * @return Validation report.
+    */
   def validate(model: BaseUnit): ClientFuture[ValidationReport] = {
     WebApiParser.chainAfterInit(() => {
       Core.validate(model, ProfileNames.RAML08, MessageStyles.RAML).asInternal
     }).asClient
   }
 
+  /** Resolves parsed RAML 0.8 model.
+    *
+    * Resolution process includes resolving references to all types, libraries, etc.
+    *
+    * @param model Parsed model.
+    * @return Resolved parsed model.
+    */
   def resolve(model: BaseUnit): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       Future {
@@ -126,9 +188,16 @@ object Raml08 {
   }
 }
 
+/** Provides methods for OAS 2.0 processing */
 @JSExportAll
 @JSExportTopLevel("WebApiParser.oas20")
 object Oas20 {
+
+  /** Parses OAS 2.0 JSON content from string or url.
+    *
+    * @param urlOrContent File url/path or content string.
+    * @return Parsed model.
+    */
   def parse(urlOrContent: String): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       if(WebApiParser.isPath(urlOrContent)) {
@@ -139,24 +208,46 @@ object Oas20 {
     }).asClient
   }
 
+  /** Generates file with OAS 2.0 JSON content.
+    *
+    * @param model Parsed model.
+    * @param url Generated file path.
+    */
   def generateFile(model: BaseUnit, url: String): ClientFuture[Unit] = {
     WebApiParser.chainAfterInit(() => {
       new Oas20Renderer().generateFile(model, url).asInternal
     }).asClient
   }
 
+  /** Generates string with OAS 2.0 JSON content.
+    *
+    * @param model Parsed model.
+    * @return Generated string.
+    */
   def generateString(model: BaseUnit): ClientFuture[String] = {
     WebApiParser.chainAfterInit(() => {
       new Oas20Renderer().generateString(model).asInternal
     }).asClient
   }
 
+  /** Validates parsed OAS 2.0 model.
+    *
+    * @param model Parsed model.
+    * @return Validation report.
+    */
   def validate(model: BaseUnit): ClientFuture[ValidationReport] = {
     WebApiParser.chainAfterInit(() => {
       Core.validate(model, ProfileNames.OAS20, MessageStyles.OAS).asInternal
     }).asClient
   }
 
+  /** Resolves parsed OAS 2.0 model.
+    *
+    * Resolution process includes resolving references to all types, libraries, etc.
+    *
+    * @param model Parsed model.
+    * @return Resolved parsed model.
+    */
   def resolve(model: BaseUnit): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       Future {
@@ -166,6 +257,11 @@ object Oas20 {
     }).asClient
   }
 
+  /** Parses OAS 2.0 YAML content from string or url.
+    *
+    * @param urlOrContent File url/path or content string.
+    * @return Parsed model.
+    */
   def parseYaml(urlOrContent: String): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       if(WebApiParser.isPath(urlOrContent)) {
@@ -177,9 +273,16 @@ object Oas20 {
   }
 }
 
+/** Provides methods for AMF Graph processing */
 @JSExportAll
 @JSExportTopLevel("WebApiParser.amfGraph")
 object AmfGraph {
+
+  /** Parses AMF Graph content from string or url.
+    *
+    * @param urlOrContent File url/path or content string.
+    * @return Parsed model.
+    */
   def parse(urlOrContent: String): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       if(WebApiParser.isPath(urlOrContent)) {
@@ -190,24 +293,46 @@ object AmfGraph {
     }).asClient
   }
 
+  /** Generates file with AMF Graph content.
+    *
+    * @param model Parsed model.
+    * @param url Generated file path.
+    */
   def generateFile(model: BaseUnit, url: String): ClientFuture[Unit] = {
     WebApiParser.chainAfterInit(() => {
       new AmfGraphRenderer().generateFile(model, url).asInternal
     }).asClient
   }
 
+  /** Generates string with AMF Graph content.
+    *
+    * @param model Parsed model.
+    * @return Generated string.
+    */
   def generateString(model: BaseUnit): ClientFuture[String] = {
     WebApiParser.chainAfterInit(() => {
       new AmfGraphRenderer().generateString(model).asInternal
     }).asClient
   }
 
+  /** Validates parsed AMF Graph model.
+    *
+    * @param model Parsed model.
+    * @return Validation report.
+    */
   def validate(model: BaseUnit): ClientFuture[ValidationReport] = {
     WebApiParser.chainAfterInit(() => {
       Core.validate(model, ProfileNames.AMF, MessageStyles.AMF).asInternal
     }).asClient
   }
 
+  /** Resolves parsed AMF Graph model.
+    *
+    * Resolution process includes resolving references to all types, libraries, etc.
+    *
+    * @param model Parsed model.
+    * @return Resolved parsed model.
+    */
   def resolve(model: BaseUnit): ClientFuture[BaseUnit] = {
     WebApiParser.chainAfterInit(() => {
       Future {
