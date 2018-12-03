e92fc484c339ba2c90a3fbd02ec4c2b436ad08f6
Artem Kostiuk <postatum@gmail.com>
2018-12-03 12:16:14 +0200

    Replace docs generation readme with script

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Generates:
+#   - JS API docs (/webapi-parser/docs/js)
+#   - Java API docs (/webapi-parser/docs/java)
+#   - AMF Model docs (/webapi-parser/docs/js)
+#
+# NOTES:
+#   - Call this script from the root of webapi-parser project;
+#   - Option --ignoreCompilerErrors is used in typedoc command
+#     to ignore incorrect typings of dependencies;
+#   - Because of this issue: https://github.com/TypeStrong/typedoc/issues/319
+#     it's currently only possible to include or exclude ALL
+#     dependencies from typedoc generation;
+
+mkdir -p docs/java
+mkdir -p docs/js/tmp_module
+
+
+# Java Docs
+echo "Generating Java docs"
+sbt generateJavadocs
+cp -r ./jvm/target/scala-2.12/genjavadoc-api/* ./docs/java
+
+
+# JS Docs
+echo "Generating JS docs"
+cp ./js/module/typings/webapi-parser.d.ts ./docs/js/tmp_module/webapi-parser.d.ts
+
+cd ./docs/js/tmp_module
+
+npm install -g typedoc
+npm install amf-client-js
+
+mkdir -p ./node_modules/@types/amf-client-js/
+cp ./node_modules/amf-client-js/typings/amf-client-js.d.ts ./node_modules/@types/amf-client-js/amf-client-js.d.ts
+typedoc --out ./gendocs ./webapi-parser.d.ts --includeDeclarations --mode file --ignoreCompilerErrors
+
+cd ..
+cp -r ./tmp_module/gendocs/* ./
+rm -rf ./tmp_module
