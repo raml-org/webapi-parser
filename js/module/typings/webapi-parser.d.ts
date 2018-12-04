d1408ad569a8f07c2f53003986469be3181d253f
Artem Kostiuk <postatum@gmail.com>
2018-12-04 12:23:03 +0200

    Use names import in TS typings
    Fixes links generation via typedoc.

--- js/module/typings/webapi-parser.d.ts
+++ js/module/typings/webapi-parser.d.ts
@@ -1,57 +1,56 @@
-import * as amf from 'amf-client-js';
+import {model, client} from 'amf-client-js';
 
 declare module "webapi-parser" {
-
     namespace WebApiParser {
 
         export class raml10 {
-            static parse(urlOrContent: string): Promise<amf.model.document.BaseUnit>
+            static parse(urlOrContent: string): Promise<model.document.BaseUnit>
 
-            static generateFile(model: amf.model.document.BaseUnit, url: string): Promise<void>
+            static generateFile(model: model.document.BaseUnit, url: string): Promise<void>
 
-            static generateString(model: amf.model.document.BaseUnit): Promise<string>
+            static generateString(model: model.document.BaseUnit): Promise<string>
 
-            static validate(model: amf.model.document.BaseUnit): Promise<amf.client.validate.ValidationReport>
+            static validate(model: model.document.BaseUnit): Promise<client.validate.ValidationReport>
 
-            static resolve(model: amf.model.document.BaseUnit): Promise<amf.model.document.BaseUnit>
+            static resolve(model: model.document.BaseUnit): Promise<model.document.BaseUnit>
         }
 
         export class raml08 {
-            static parse(urlOrContent: string): Promise<amf.model.document.BaseUnit>
+            static parse(urlOrContent: string): Promise<model.document.BaseUnit>
 
-            static generateFile(model: amf.model.document.BaseUnit, url: string): Promise<void>
+            static generateFile(model: model.document.BaseUnit, url: string): Promise<void>
 
-            static generateString(model: amf.model.document.BaseUnit): Promise<string>
+            static generateString(model: model.document.BaseUnit): Promise<string>
 
-            static validate(model: amf.model.document.BaseUnit): Promise<amf.client.validate.ValidationReport>
+            static validate(model: model.document.BaseUnit): Promise<client.validate.ValidationReport>
 
-            static resolve(model: amf.model.document.BaseUnit): Promise<amf.model.document.BaseUnit>
+            static resolve(model: model.document.BaseUnit): Promise<model.document.BaseUnit>
         }
 
         export class oas20 {
-            static parse(urlOrContent: string): Promise<amf.model.document.BaseUnit>
+            static parse(urlOrContent: string): Promise<model.document.BaseUnit>
 
-            static generateFile(model: amf.model.document.BaseUnit, url: string): Promise<void>
+            static generateFile(model: model.document.BaseUnit, url: string): Promise<void>
 
-            static generateString(model: amf.model.document.BaseUnit): Promise<string>
+            static generateString(model: model.document.BaseUnit): Promise<string>
 
-            static validate(model: amf.model.document.BaseUnit): Promise<amf.client.validate.ValidationReport>
+            static validate(model: model.document.BaseUnit): Promise<client.validate.ValidationReport>
 
-            static resolve(model: amf.model.document.BaseUnit): Promise<amf.model.document.BaseUnit>
+            static resolve(model: model.document.BaseUnit): Promise<model.document.BaseUnit>
 
-            static parseYaml(urlOrContent: string): Promise<amf.model.document.BaseUnit>
+            static parseYaml(urlOrContent: string): Promise<model.document.BaseUnit>
         }
 
         export class amfGraph {
-            static parse(urlOrContent: string): Promise<amf.model.document.BaseUnit>
+            static parse(urlOrContent: string): Promise<model.document.BaseUnit>
 
-            static generateFile(model: amf.model.document.BaseUnit, url: string): Promise<void>
+            static generateFile(model: model.document.BaseUnit, url: string): Promise<void>
 
-            static generateString(model: amf.model.document.BaseUnit): Promise<string>
+            static generateString(model: model.document.BaseUnit): Promise<string>
 
-            static validate(model: amf.model.document.BaseUnit): Promise<amf.client.validate.ValidationReport>
+            static validate(model: model.document.BaseUnit): Promise<client.validate.ValidationReport>
 
-            static resolve(model: amf.model.document.BaseUnit): Promise<amf.model.document.BaseUnit>
+            static resolve(model: model.document.BaseUnit): Promise<model.document.BaseUnit>
         }
     }
 }
