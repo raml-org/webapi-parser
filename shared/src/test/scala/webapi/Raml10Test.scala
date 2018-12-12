c02602fbeb215049c8de30faf65e66e8222ccb06
Artem Kostiuk <postatum@gmail.com>
2018-12-12 13:03:41 +0200

    Finish writing raml10 tests

--- shared/src/test/scala/webapi/Raml10Test.scala
+++ shared/src/test/scala/webapi/Raml10Test.scala
@@ -1,22 +1,25 @@
 package webapi
 
 import amf.client.model.document.{Document, BaseUnit}
-import amf.client.model.domain.{WebApi}
+import amf.client.model.domain.{WebApi, EndPoint, Response, Operation, Payload}
 import amf.client.convert.CoreClientConverters._
 
 import org.scalatest.AsyncFunSuite
 import org.scalatest.Matchers
 import org.scalatest.Assertions._
 
-import scala.concurrent._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.io.Source
 
 
 class Raml10Test extends AsyncFunSuite with Matchers {
 
   override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
-  private val strInp: String =
+  private val validFilePath     = "file://shared/src/test/resources/raml/api-with-types.raml"
+  private val invalidFilePath   = "file://shared/src/test/resources/raml/invalid-examples.raml"
+  private val generatedFilePath = s"${System.getProperty("java.io.tmpdir")}/generated.raml"
+  private val apiString: String =
     """#%RAML 1.0
       |title: API with Types
       |types:
@@ -37,16 +40,74 @@ class Raml10Test extends AsyncFunSuite with Matchers {
       |          application/json:
       |            type: User""".stripMargin
 
-  test("Test RAML 1.0 string parsing") {
+  test("String parsing") {
     for {
-      unit <- Raml10.parse(strInp).asInternal
+      unit <- Raml10.parse(apiString).asInternal
     } yield {
       val bu: BaseUnit = unit
       val doc = bu.asInstanceOf[Document]
       val api = doc.encodes.asInstanceOf[WebApi]
       doc.declares should have size 1
       api.endPoints should have size 1
-      api.name.value() should be("API with Types")
+      api.name.value() should be ("API with Types")
+    }
+  }
+
+  test("File parsing") {
+    for {
+      unit <- Raml10.parse(validFilePath).asInternal
+    } yield {
+      val bu: BaseUnit = unit
+      val doc = bu.asInstanceOf[Document]
+      val api = doc.encodes.asInstanceOf[WebApi]
+      doc.declares should have size 1
+      api.endPoints should have size 1
+      api.name.value() should be ("API with Types")
+    }
+  }
+
+  test("File generation") {
+    for {
+      unit <- Raml10.parse(apiString).asInternal
+      _ <- Raml10.generateFile(unit, s"file://${generatedFilePath}").asInternal
+    } yield {
+      val gen = Source.fromFile(generatedFilePath).getLines.toList
+      gen should contain ("#%RAML 1.0")
+      gen should contain ("/users/{id}:")
+    }
+  }
+
+  test("String generation") {
+    for {
+      unit <- Raml10.parse(apiString).asInternal
+      genStr <- Raml10.generateString(unit).asInternal
+    } yield {
+      val gen = genStr.split("\n").map(_.trim)
+      gen should contain ("#%RAML 1.0")
+      gen should contain ("/users/{id}:")
+    }
+  }
+
+  test("Validation") {
+    for {
+      unit <- Raml10.parse(invalidFilePath).asInternal
+      report <- Raml10.validate(unit).asInternal
+    } yield {
+      report.conforms should be (false)
+      report.results should have size 3
+    }
+  }
+
+  test("Resolution") {
+    for {
+      unit <- Raml10.parse(apiString).asInternal
+      resolved <- Raml10.resolve(unit).asInternal
+      genStr <- Raml10.generateString(resolved).asInternal
+    } yield {
+      val gen = genStr.split("\n").map(_.trim)
+      gen should contain ("firstName:")
+      gen should not contain ("type: User")
+      gen should not contain ("types:")
     }
   }
 }
