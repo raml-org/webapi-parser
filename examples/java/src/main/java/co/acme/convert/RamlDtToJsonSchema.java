0c40a569f7ba92516d9a2b63f23ebd8a3e101860
Artem Kostiuk <postatum@gmail.com>
2019-02-09 12:58:23 +0200

    Add ramldt>json Java examples

---
+++ examples/java/src/main/java/co/acme/convert/RamlDtToJsonSchema.java
@@ -0,0 +1,70 @@
+package co.acme.convert;
+
+import webapi.Conversion;
+
+import java.util.concurrent.ExecutionException;
+
+public class RamlDtToJsonSchema {
+
+  // Example of converting single RAML Data Type from RAML 1.0 API
+  public static void convertFromApi() throws InterruptedException, ExecutionException {
+    String inp ="#%RAML 1.0\n" +
+                "title: API with Types\n" +
+                "types:\n" +
+                "  Book:\n" +
+                "    type: object\n" +
+                "    properties:\n" +
+                "      title: string\n" +
+                "      author: string\n" +
+                "  User:\n" +
+                "    type: object\n" +
+                "    properties:\n" +
+                "      username: string\n";
+    System.out.println("Input Raml10 API:\n" + inp);
+    String converted = Conversion.toJsonSchema(inp, "User").get();
+    System.out.println("Converted to JSON Schema:\n" + converted);
+  }
+
+  // Example of converting single RAML Data Type from RAML 1.0 Library
+  public static void convertFromLibrary() throws InterruptedException, ExecutionException {
+    String inp ="#%RAML 1.0 Library\n" +
+                "types:\n" +
+                "  Book:\n" +
+                "    type: object\n" +
+                "    properties:\n" +
+                "      title: string\n" +
+                "      author: string\n";
+    System.out.println("Input Raml10 Library:\n" + inp);
+    String converted = Conversion.toJsonSchema(inp, "Book").get();
+    System.out.println("Converted to JSON Schema:\n" + converted);
+  }
+
+  // Example of converting single RAML Data Type from RAML 1.0 DataType Fragment
+  public static void convertFromDataType() throws InterruptedException, ExecutionException {
+    String inp ="#%RAML 1.0 DataType\n" +
+                "type: object\n" +
+                "properties:\n" +
+                "  title: string\n" +
+                "  author: string\n";
+    System.out.println("Input Raml10 DataType:\n" + inp);
+    String converted = Conversion.toJsonSchema(inp, "AnyName").get();
+    System.out.println("Converted to JSON Schema:\n" + converted);
+  }
+
+  // Example of converting inexisting type from RAML 1.0 library
+  public static void convertInexisting() throws InterruptedException, ExecutionException {
+    String inp ="#%RAML 1.0 Library\n" +
+                "types:\n" +
+                "  Book:\n" +
+                "    type: object\n" +
+                "    properties:\n" +
+                "      title: string\n" +
+                "      author: string\n";
+    System.out.println("Input Raml10 Library:\n" + inp);
+    try {
+      String converted = Conversion.toJsonSchema(inp, "FOOBAR").get();
+    } catch (Exception e) {
+      System.out.println("Tried to convert inexisting type from API:\n" + e.toString());
+    }
+  }
+}
--- examples/java/src/main/java/co/acme/demo/WebApiParserDemo.java
+++ examples/java/src/main/java/co/acme/demo/WebApiParserDemo.java
@@ -12,6 +12,7 @@ import co.acme.validate.Raml10Validation;
 import co.acme.validate.Raml08Validation;
 import co.acme.validate.Oas20Validation;
 import co.acme.validate.AmfGraphValidation;
+import co.acme.convert.RamlDtToJsonSchema;
 
 import java.util.concurrent.ExecutionException;
 
@@ -47,5 +48,10 @@ public class WebApiParserDemo {
     Raml08Validation.validate();
     Oas20Validation.validate();
     AmfGraphValidation.validate();
+
+    RamlDtToJsonSchema.convertFromApi();
+    RamlDtToJsonSchema.convertFromLibrary();
+    RamlDtToJsonSchema.convertFromDataType();
+    RamlDtToJsonSchema.convertInexisting();
   }
 }
