2f77789552c3f391226f01a3f54a338532d53b4f
Artem Kostiuk <postatum@gmail.com>
2019-02-22 12:39:33 +0200

    Parse file instead of str in json->raml example

--- examples/js/convert-jsonschema-ramldt.js
+++ examples/js/convert-jsonschema-ramldt.js
@@ -2,77 +2,25 @@
  * Example of converting JSON Schema to RAML Data Type using AMF Model
  */
 const wap = require('webapi-parser').WebApiParser
+const path = require('path')
 
-const oasDocument = `
-  {
-    "swagger": "2.0",
-    "info": {
-      "version": "1.0.0",
-      "title": "Swagger Petstore",
-      "license": {"name": "MIT"}
-    },
-    "host": "petstore.swagger.io",
-    "basePath": "/v1",
-    "schemes": ["http"],
-    "consumes": ["application/json"],
-    "produces": ["application/json"],
-    "paths": {
-      "/pets/{petId}": {
-        "get": {
-          "responses": {
-            "200": {
-              "description": "Expected response to a valid request",
-              "schema": {"$ref": "#/definitions/Pet"}
-            }
-          }
-        }
-      },
-      "/pets/{petId}/owner": {
-        "get": {
-          "responses": {
-            "200": {
-              "schema": {"$ref": "#/definitions/Owner"}
-            }
-          }
-        }
-      }
-    },
-    "definitions": {
-      "Pet": {
-        "required": ["id", "name"],
-        "properties": {
-          "id": {"type": "integer", "format": "int64"},
-          "name": {"type": "string"},
-          "tag": {"type": "string"}
-        }
-      },
-      "Owner": {
-        "type": "object",
-        "properties": {
-          "name": {"type": "string"},
-          "pet": {"$ref": "#/definitions/Pet"}
-        }
-      }
-    }
-  }
-`
 
 async function main () {
   // Parse an API document string
-  const model = await wap.oas20.parse(oasDocument)
+  const inPath = path.join(__dirname, '../api-specs/oas/pet-store-api.json')
+  const docPath = `file://${inPath}`
+  const model = await wap.oas20.parse(docPath)
 
   // Convert type from "definitions".
   // Type can be picked using utility functions.
   console.log(
     'RAML Data Type from definitions using util:\n',
-    model.findById(
-      'http://a.ml/amf/default_document#/declarations/types/Pet'
-    ).toRamlDatatype)
+    model.findById(`${docPath}#/declarations/types/Pet`).toJsonSchema)
 
   // Type can also be picked by index.
   console.log(
     'RAML Data Type from definitions by index:\n',
-    model.declares[0].toRamlDatatype)
+    model.declares[0].toJsonSchema)
 
   // To properly convert type with references, model needs to be
   // resolved first.
@@ -85,7 +33,7 @@ async function main () {
       .operations[0]
       .responses[0]
       .payloads[0]
-      .schema.toRamlDatatype)
+      .schema.toJsonSchema)
 }
 
 main()
