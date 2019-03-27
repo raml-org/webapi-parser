fb584a233ad3df444fe63a5063701859d9095ff9
Artem Kostiuk <postatum@gmail.com>
2019-03-27 13:04:24 +0200

    Define few more custom classes

--- shared/src/main/scala/webapi/BaseUnit.scala
+++ shared/src/main/scala/webapi/BaseUnit.scala
@@ -1,12 +1,42 @@
 package webapi
 
 import amf.client.model.document.{
-  BaseUnit, Document, Module
+  BaseUnit,
+  Document,
+  Module,
+  // PayloadFragment,
+  ExternalFragment,
+  Vocabulary,
+  Extension,
+  Overlay,
+  DocumentationItem,
+  DataType,
+  NamedExample,
+  ResourceTypeFragment,
+  TraitFragment,
+  AnnotationTypeDeclaration,
+  SecuritySchemeFragment
 }
 import amf.core.model.document.{
   BaseUnit => InternalBaseUnit,
   Document => InternalDocument,
-  Module => InternalModule
+  Module => InternalModule,
+  // PayloadFragment => InternalPayloadFragment,
+  ExternalFragment => InternalExternalFragment
+}
+import amf.plugins.document.vocabularies.model.document.{
+  Vocabulary => InternalVocabulary
+}
+import amf.plugins.document.webapi.model.{
+  Extension => InternalExtension,
+  Overlay => InternalOverlay,
+  DocumentationItemFragment => InternalDocumentationItemFragment,
+  DataTypeFragment => InternalDataTypeFragment,
+  NamedExampleFragment => InternalNamedExampleFragment,
+  ResourceTypeFragment => InternalResourceTypeFragment,
+  TraitFragment => InternalTraitFragment,
+  AnnotationTypeDeclarationFragment => InternalAnnotationTypeDeclarationFragment,
+  SecuritySchemeFragment => InternalSecuritySchemeFragment
 }
 
 import scala.scalajs.js.annotation._
@@ -23,8 +53,25 @@ trait WebApiBaseUnit extends BaseUnit {
 
 @JSExportAll
 @JSExportTopLevel("webapi.WebApiDocument")
-class WebApiDocument(override val _internal: InternalDocument) extends Document(_internal) with WebApiBaseUnit
+class WebApiDocument(override val _internal: InternalDocument)
+    extends Document(_internal) with WebApiBaseUnit
 
 @JSExportAll
 @JSExportTopLevel("webapi.WebApiModule")
-class WebApiModule(override val _internal: InternalModule) extends Module(_internal) with WebApiBaseUnit
+class WebApiModule(override val _internal: InternalModule)
+    extends Module(_internal) with WebApiBaseUnit
+
+@JSExportAll
+@JSExportTopLevel("webapi.WebApiExternalFragment")
+class WebApiExternalFragment(override val _internal: InternalExternalFragment)
+    extends ExternalFragment(_internal) with WebApiBaseUnit
+
+@JSExportAll
+@JSExportTopLevel("webapi.WebApiExtension")
+class WebApiExtension(override val _internal: InternalExtension)
+    extends Extension(_internal) with WebApiBaseUnit
+
+@JSExportAll
+@JSExportTopLevel("webapi.WebApiOverlay")
+class WebApiOverlay(override val _internal: InternalOverlay)
+    extends Overlay(_internal) with WebApiBaseUnit
