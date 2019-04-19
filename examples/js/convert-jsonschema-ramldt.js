a9878489326c3dea6db477c47348cb6411f6ab8f
Artem Kostiuk <postatum@gmail.com>
2019-04-19 09:31:14 +0300

    Rework js example of json>raml

--- examples/js/convert-jsonschema-ramldt.js
+++ examples/js/convert-jsonschema-ramldt.js
@@ -1,39 +1,43 @@
 /**
- * Example of converting JSON Schema to RAML Data Type using AMF Model
+ * Example of converting JSON Schema to RAML Data Type using WebApi Model.
  */
 const wap = require('webapi-parser').WebApiParser
 const path = require('path')
 
+const schema = `
+  {
+    "$schema": "http://json-schema.org/draft-04/schema",
+    "type": "object",
+    "required": ["firstName", "lastName", "age"],
+    "properties": {
+      "firstName": {"type": "string"},
+      "lastName": {"type": "string"},
+      "age": {"type": "integer", "minimum": 0, "maximum": 99}
+    }
+  }
+`
 
 async function main () {
+  const parsedSchema = {
+    openapi: '2.0',
+    definitions: {
+      User: JSON.parse(schema)
+    }
+  }
+
   // Parse an API document string
-  const inPath = path.join(__dirname, '../api-specs/oas/pet-store-api.json')
-  const docPath = `file://${inPath}`
-  const model = await wap.oas20.parse(docPath)
+  const model = await wap.oas20.parse(JSON.stringify(parsedSchema))
 
   // Convert type from "definitions".
   // Type can be picked using utility functions.
   console.log(
     'RAML Data Type from definitions using util:\n',
-    model.findById(`${docPath}#/declarations/types/Pet`).toRamlDatatype)
+    model.getDeclarationByName('User').toRamlDatatype)
 
   // Type can also be picked by index.
   console.log(
     'RAML Data Type from definitions by index:\n',
     model.declares[0].toRamlDatatype)
-
-  // To properly convert type with references, model needs to be
-  // resolved first.
-  const resolved = await wap.oas20.resolve(model)
-  console.log(
-    'RAML Data Type (with references) from resolved document:\n',
-    resolved
-      .encodes
-      .endPoints[1]
-      .operations[0]
-      .responses[0]
-      .payloads[0]
-      .schema.toRamlDatatype)
 }
 
 main()
