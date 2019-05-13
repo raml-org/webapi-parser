d5a12cddbb9261565fbe22d297438a0dacc896de
Artem Kostiuk <postatum@gmail.com>
2019-05-13 12:57:02 +0300

    Do not run sonatypeRelease for snapshots

--- scripts/publish-java.sh
+++ scripts/publish-java.sh
@@ -14,5 +14,18 @@ sbt webapiJVM/package
 echo "Uploading artifact to Sonatype staging"
 sbt webapiJVM/publishSigned
 
-echo "Promoting the release to be ready for synching to Maven Central"
-sbt sonatypeRelease
+# Find the first build.sbt in the path tree of this script,
+# find the version line and extract the content within double quotes
+PROJECT_VERSION=`find '.' -name "build.sbt" |
+    head -n1 |
+    xargs grep '[ \t]*ThisBuild / version :=' |
+    head -n1 |
+    sed 's/.*"\(.*\)".*/\1/' 2> /dev/null`
+
+# If the project version has "SNAPSHOT" suffix, the project will be
+# published to the snapshot repository of Sonatype, and we cannot use
+# sonatypeRelease command.
+if ! [[ ${PROJECT_VERSION} == *-SNAPSHOT ]]; then
+    echo "Promoting the release to be ready for synching to Maven Central"
+    sbt sonatypeRelease
+fi
