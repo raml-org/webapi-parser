a418159746f39ce0fed6980ac54a26bc1b6492e8
Artem Kostiuk <postatum@gmail.com>
2019-05-14 08:20:14 +0300

    Fixes #8. Fix amf graph string parsing example

--- examples/java/src/main/java/co/acme/parse/AmfGraphParsing.java
+++ examples/java/src/main/java/co/acme/parse/AmfGraphParsing.java
@@ -19,33 +19,64 @@ public class AmfGraphParsing {
 
   // Example of parsing AMF Graph string
   public static void parseString() throws InterruptedException, ExecutionException {
-    String inp ="[\n" +
-                "  {\n" +
-                "    \"@id\": \"\",\n" +
+    String inp ="[{\n" +
+                "  \"@id\": \"http://a.ml/amf/default_document\",\n" +
+                "  \"@type\": [\n" +
+                "    \"http://a.ml/vocabularies/document#Document\",\n" +
+                "    \"http://a.ml/vocabularies/document#Fragment\",\n" +
+                "    \"http://a.ml/vocabularies/document#Module\",\n" +
+                "    \"http://a.ml/vocabularies/document#Unit\"\n" +
+                "  ],\n" +
+                "  \"http://a.ml/vocabularies/document#encodes\": [{\n" +
+                "    \"@id\": \"http://a.ml/amf/default_document#/web-api\",\n" +
                 "    \"@type\": [\n" +
-                "      \"http://raml.org/vocabularies/document#Document\",\n" +
-                "      \"http://raml.org/vocabularies/document#Fragment\",\n" +
-                "      \"http://raml.org/vocabularies/document#Module\",\n" +
-                "      \"http://raml.org/vocabularies/document#Unit\"\n" +
+                "      \"http://schema.org/WebAPI\",\n" +
+                "      \"http://a.ml/vocabularies/document#RootDomainElement\",\n" +
+                "      \"http://a.ml/vocabularies/document#DomainElement\"\n" +
                 "    ],\n" +
-                "    \"http://raml.org/vocabularies/document#encodes\": [\n" +
-                "      {\n" +
-                "        \"@id\": \"#/web-api\",\n" +
+                "    \"http://schema.org/name\": [{\"@value\": \"API with Types\"}],\n" +
+                "    \"http://a.ml/vocabularies/http#endpoint\": [{\n" +
+                "      \"@id\": \"http://a.ml/amf/default_document#/web-api/end-points//users/{id}\",\n" +
+                "      \"@type\": [\n" +
+                "        \"http://a.ml/vocabularies/http#EndPoint\",\n" +
+                "        \"http://a.ml/vocabularies/document#DomainElement\"\n" +
+                "      ],\n" +
+                "      \"http://a.ml/vocabularies/http#path\": [{\"@value\": \"/users/{id}\"}],\n" +
+                "      \"http://www.w3.org/ns/hydra/core#supportedOperation\": [{\n" +
+                "        \"@id\": \"http://a.ml/amf/default_document#/web-api/end-points//users/{id}/get\",\n" +
                 "        \"@type\": [\n" +
-                "          \"http://schema.org/WebAPI\",\n" +
-                "          \"http://raml.org/vocabularies/document#DomainElement\"\n" +
+                "          \"http://www.w3.org/ns/hydra/core#Operation\",\n" +
+                "          \"http://a.ml/vocabularies/document#DomainElement\"\n" +
                 "        ],\n" +
-                "        \"http://schema.org/name\": [\n" +
-                "          {\n" +
-                "            \"@value\": \"ACME Banking HTTP API\"\n" +
-                "          }\n" +
+                "        \"http://www.w3.org/ns/hydra/core#method\": [{\"@value\": \"get\"}]\n" +
+                "      }],\n" +
+                "      \"http://a.ml/vocabularies/http#parameter\": [{\n" +
+                "        \"@id\": \"http://a.ml/amf/default_document#/web-api/end-points//users/{id}/parameter/id\",\n" +
+                "        \"@type\": [\n" +
+                "          \"http://a.ml/vocabularies/http#Parameter\",\n" +
+                "          \"http://a.ml/vocabularies/document#DomainElement\"\n" +
                 "        ],\n" +
-                "        \"http://raml.org/vocabularies/http#host\": [\n" +
-                "          {\n" +
-                "            \"@value\": \"acme-banking.com\"\n" +
-                "          }\n" +
-                "        ]" +
-                "}]}]";
+                "        \"http://schema.org/name\": [{\"@value\": \"id\"}],\n" +
+                "        \"http://a.ml/vocabularies/http#paramName\": [{\"@value\": \"id\"}],\n" +
+                "        \"http://www.w3.org/ns/hydra/core#required\": [{\"@value\": true}],\n" +
+                "        \"http://a.ml/vocabularies/http#binding\": [{\"@value\": \"path\"}],\n" +
+                "        \"http://a.ml/vocabularies/http#schema\": [{\n" +
+                "          \"@id\": \"http://a.ml/amf/default_document#/web-api/end-points//users/{id}/parameter/id/scalar/id\",\n" +
+                "          \"@type\": [\n" +
+                "            \"http://a.ml/vocabularies/shapes#ScalarShape\",\n" +
+                "            \"http://www.w3.org/ns/shacl#Shape\",\n" +
+                "            \"http://a.ml/vocabularies/shapes#Shape\",\n" +
+                "            \"http://a.ml/vocabularies/document#DomainElement\"\n" +
+                "          ],\n" +
+                "          \"http://www.w3.org/ns/shacl#datatype\": [\n" +
+                "            {\"@id\": \"http://www.w3.org/2001/XMLSchema#string\"}\n" +
+                "          ],\n" +
+                "          \"http://www.w3.org/ns/shacl#name\": [{\"@value\": \"id\"}]\n" +
+                "        }]\n" +
+                "      }]\n" +
+                "    }]\n" +
+                "  }]\n" +
+                "}]";
     System.out.println("Input AMF Graph string:\n" + inp);
 
     // Parse the string
