e28a49e17a0ffba57fdd4a0f453507f8a2710aee
Artem Kostiuk <postatum@gmail.com>
2019-05-14 13:46:18 +0300

    Try to set sonatypeProfileName in Common.scala

--- project/Common.scala
+++ project/Common.scala
@@ -1,5 +1,6 @@
 import sbt.Keys.{scalacOptions, _}
 import sbt.{Def, _}
+import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
 
 object Common {
 
@@ -13,6 +14,7 @@ object Common {
   )
 
   val publish: Seq[Def.Setting[_]] = Seq(
+    sonatypeProfileName := "org.raml",
     publishTo := {
       val nexus = "https://oss.sonatype.org/"
       if (isSnapshot.value) Some("snapshots" at nexus + "content/repositories/snapshots")
--- sonatype.sbt
+++
@@ -1,4 +0,0 @@
-import xerial.sbt.Sonatype._
-
-// Profile name of the sonatype account. The default is the same with the organization value
-sonatypeProfileName := "org.raml"
