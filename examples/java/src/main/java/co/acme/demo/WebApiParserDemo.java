9cfe61672a6d3926e9b73ba446b7d1866ca0459d
Artem Kostiuk <postatum@gmail.com>
2020-06-02 12:00:57 +0300

    Add resolution examples

--- examples/java/src/main/java/co/acme/demo/WebApiParserDemo.java
+++ examples/java/src/main/java/co/acme/demo/WebApiParserDemo.java
@@ -18,6 +18,7 @@ import co.acme.validate.AmfGraphValidation;
 import co.acme.model.Raml10Building;
 import co.acme.model.Raml10Navigation;
 import co.acme.model.Raml10UtilityMethods;
+import co.acme.model.Raml10Resolution;
 import co.acme.translate.RamlDtToJsonSchema;
 import co.acme.translate.JsonSchemaToRamlDt;
 
@@ -27,59 +28,60 @@ import java.io.UnsupportedEncodingException;
 // Runs all the example classes
 public class WebApiParserDemo {
   public static void main(String[] args) throws ExecutionException, InterruptedException, UnsupportedEncodingException {
-    Raml10Parsing.parseStringWithLocation();
-    Raml10Parsing.parseString();
-    Raml10Parsing.parseFile();
+    // Raml10Parsing.parseStringWithLocation();
+    // Raml10Parsing.parseString();
+    // Raml10Parsing.parseFile();
 
-    Raml08Parsing.parseString();
-    Raml08Parsing.parseFile();
+    // Raml08Parsing.parseString();
+    // Raml08Parsing.parseFile();
 
-    Oas20Parsing.parseString();
-    Oas20Parsing.parseFile();
-    Oas20Parsing.parseYamlString();
-    Oas20Parsing.parseYamlFile();
+    // Oas20Parsing.parseString();
+    // Oas20Parsing.parseFile();
+    // Oas20Parsing.parseYamlString();
+    // Oas20Parsing.parseYamlFile();
 
-    Oas30Parsing.parseString();
-    Oas30Parsing.parseFile();
-    Oas30Parsing.parseYamlString();
-    Oas30Parsing.parseYamlFile();
+    // Oas30Parsing.parseString();
+    // Oas30Parsing.parseFile();
+    // Oas30Parsing.parseYamlString();
+    // Oas30Parsing.parseYamlFile();
 
-    AmfGraphParsing.parseString();
-    AmfGraphParsing.parseFile();
+    // AmfGraphParsing.parseString();
+    // AmfGraphParsing.parseFile();
 
-    Raml10Generation.generateString();
-    Raml10Generation.generateFile();
+    // Raml10Generation.generateString();
+    // Raml10Generation.generateFile();
 
-    Raml08Generation.generateString();
-    Raml08Generation.generateFile();
+    // Raml08Generation.generateString();
+    // Raml08Generation.generateFile();
 
-    Oas20Generation.generateString();
-    Oas20Generation.generateFile();
-    Oas20Generation.generateYamlString();
-    Oas20Generation.generateYamlFile();
+    // Oas20Generation.generateString();
+    // Oas20Generation.generateFile();
+    // Oas20Generation.generateYamlString();
+    // Oas20Generation.generateYamlFile();
 
-    Oas30Generation.generateString();
-    Oas30Generation.generateFile();
-    Oas30Generation.generateYamlString();
-    Oas30Generation.generateYamlFile();
+    // Oas30Generation.generateString();
+    // Oas30Generation.generateFile();
+    // Oas30Generation.generateYamlString();
+    // Oas30Generation.generateYamlFile();
 
-    AmfGraphGeneration.generateString();
-    AmfGraphGeneration.generateFile();
+    // AmfGraphGeneration.generateString();
+    // AmfGraphGeneration.generateFile();
 
-    Raml10Validation.validate();
-    Raml08Validation.validate();
-    Oas20Validation.validate();
-    Oas30Validation.validate();
-    AmfGraphValidation.validate();
+    // Raml10Validation.validate();
+    // Raml08Validation.validate();
+    // Oas20Validation.validate();
+    // Oas30Validation.validate();
+    // AmfGraphValidation.validate();
 
-    Raml10Building.buildApi();
-    Raml10Navigation.navigateApi();
-    Raml10UtilityMethods.navigateApi();
+    // Raml10Building.buildApi();
+    // Raml10Navigation.navigateApi();
+    // Raml10UtilityMethods.navigateApi();
+    Raml10Resolution.resolveApi();
 
-    RamlDtToJsonSchema.translateFromApi();
-    RamlDtToJsonSchema.translateFromLibrary();
-    RamlDtToJsonSchema.translateFromDataType();
+    // RamlDtToJsonSchema.translateFromApi();
+    // RamlDtToJsonSchema.translateFromLibrary();
+    // RamlDtToJsonSchema.translateFromDataType();
 
-    JsonSchemaToRamlDt.translateFromApi();
+    // JsonSchemaToRamlDt.translateFromApi();
   }
 }
